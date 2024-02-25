a825d1bb50fb65efbaf9713a9a93a6413e05b929
Jisoo Song <jeesoo9595@snu.ac.kr>
2024-02-25 18:19:11 +0900

    feat: implement JellyRoll wetting height model
    Modify jrwtf_analyzer

--- src/wettingfront_lges/jellyroll.py
+++ src/wettingfront_lges/jellyroll.py
@@ -1,9 +1,11 @@
 """Jelly roll analysis."""
 
 import os
+from importlib.metadata import entry_points
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import yaml
 
 
 def jrwtf_analyzer(name, fields):
@@ -11,12 +13,15 @@ def jrwtf_analyzer(name, fields):
 
     The analyzer defines the following fields in configuration entry:
 
+    - **model** (`str`, optional): Wetting front model, implemented by plugins.
     - **data** (`str`): CSV file containing wetting front data.
         The CSV file must contain the following fields:
         - **ImbibitionStart** (`str`): Starting time in ISO8601 format.
         - **ImbibitionEnd** (`str`): Ending time in ISO8601 format.
         - **WettingHeight** (`number`): Wetting height in milimeter.
     - **output**:
+        - **model** (`str`, optional): Path to the output YAML file.
+            The model file stores model parameters.
         - **data** (`str`, optional): Path to the output CSV file.
             The data file stores wetting front data.
         - **plot** (`str`, optional): Path to the output plot file.
@@ -30,9 +35,17 @@ def jrwtf_analyzer(name, fields):
             type: JellyRollWettingFront
             data: foo.csv
             output:
+                model: output/model.yml
                 data: output/data.csv
                 plot: output/plot.jpg
     """
+    model = fields.get("model", None)
+    if model is not None:
+        MODELS = {}
+        for ep in entry_points(group="wettingfront.models"):
+            MODELS[ep.name] = ep
+        model = MODELS[model].load()
+
     data = os.path.expandvars(fields["data"])
 
     def makedir(path):
@@ -43,6 +56,7 @@ def jrwtf_analyzer(name, fields):
         return path
 
     output = fields.get("output", {})
+    output_model = makedir(output.get("model", ""))
     output_data = makedir(output.get("data", ""))
     output_plot = makedir(output.get("plot", ""))
 
@@ -53,14 +67,31 @@ def jrwtf_analyzer(name, fields):
     idx = dt.argsort()
     time, height = dt[idx], df["WettingHeight"][idx]
 
+    if output_model:
+        if model is None:
+            params = ()
+        else:
+            func, params = model(time, height)
+            predict = func(time)
+        with open(output_model, "w") as f:
+            yaml.dump(list(float(p) for p in params), f)
+
     if output_data:
-        pd.DataFrame({"time (s)": time, "height (mm)": height}).to_csv(
-            output_data, index=False
-        )
+        if model is None:
+            pd.DataFrame({"time (s)": time, "height (mm)": height}).to_csv(
+                output_data, index=False
+            )
+        else:
+            pd.DataFrame(
+                {"time (s)": time, "height (mm)": height, "fitted height (mm)": predict}
+            ).to_csv(output_data, index=False)
 
     if output_plot:
         fig, ax = plt.subplots()
-        ax.plot(time, height, "x")
+        ax.plot(time, height, label="data")
+        if model is not None:
+            ax.plot(time, predict, label="model")
         ax.set_xlabel("Time (s)")
         ax.set_ylabel("Height (mm)")
+        ax.legend()
         fig.savefig(output_plot)
